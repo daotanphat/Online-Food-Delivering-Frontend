ee462443b77bdd7bd5ca12339bc3054d26c9b588
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-01 21:15:54 +0700

    feat: create list food to display slide

---
+++ src/component/Home/topMeels.js
@@ -0,0 +1,34 @@
+export const topMeels = [
+    {
+        image: "https://cdn.pixabay.com/photo/2014/10/19/20/59/hamburger-494706_1280.jpg",
+        title: "Hamburger"
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2023/05/31/11/15/fish-8031138_1280.jpg",
+        title: "Fillet"
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_1280.jpg",
+        title: "Meat"
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2021/11/22/06/22/italian-cuisine-6815897_1280.jpg",
+        title: "Pasta"
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2021/11/01/15/52/spring-roll-6760871_1280.jpg",
+        title: "Nem Cuon"
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_1280.jpg",
+        title: "Pancakes"
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg",
+        title: "Pizza"
+    },
+    {
+        image: "https://cdn.pixabay.com/photo/2018/03/15/12/16/food-3228057_1280.jpg",
+        title: "Nem"
+    },
+]
