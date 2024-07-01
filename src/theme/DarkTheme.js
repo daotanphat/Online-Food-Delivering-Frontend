ecb2999608a240d481deaeeea14d52fdd3cd80c3
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-01 21:19:06 +0700

    feat: create const color

---
+++ src/theme/DarkTheme.js
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+
+export const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            // main: '#e91e63'
+            main: '#002b36'
+        },
+        secondary: {
+            main: '#5A20CB'
+        },
+        black: {
+            main: '#242B2E'
+        },
+        background: {
+            main: '#000000',
+            default: '#0D0D0D',
+            paper: '#0D0D0D'
+        },
+        textColor: '#111111'
+    }
+})
