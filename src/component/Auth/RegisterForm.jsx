7057805a55e746fd4082e788bdd579dbf496d94e
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-06 23:35:19 +0700

    feat: add Modal Register

---
+++ src/component/Auth/RegisterForm.jsx
@@ -0,0 +1,80 @@
+import { Password } from '@mui/icons-material'
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material'
+import { Field, Form, Formik } from 'formik'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const initalValues = {
+    fullName: "",
+    email: "",
+    password: "",
+    role: ""
+}
+
+const RegisterForm = () => {
+    const navigate = useNavigate();
+    const handleSubmit = (values) => {
+        console.log("values: ", values);
+    }
+    return (
+        <div>
+            <Typography variant='h5' className='text-center'>
+                REGISTER
+            </Typography>
+
+            <Formik onSubmit={handleSubmit} initialValues={initalValues}>
+                <Form>
+                    <Field
+                        as={TextField}
+                        name="fullName"
+                        label="Full Name"
+                        fullWidth
+                        variant="outlined"
+                        margin="normal"
+                    />
+                    <Field
+                        as={TextField}
+                        name="email"
+                        label="Email"
+                        fullWidth
+                        variant="outlined"
+                        margin="normal"
+                    />
+                    <Field
+                        as={TextField}
+                        name="password"
+                        label="Password"
+                        fullWidth
+                        variant="outlined"
+                        margin="normal"
+                        type="password"
+                    />
+                    <FormControl fullWidth margin="normal">
+                        <InputLabel id="role-simple-select-label">Role</InputLabel>
+                        <Field
+                            as={Select}
+                            labelId="role-simple-select-label"
+                            id="role-simple-select"
+                            // value={role}
+                            label="Role"
+                            name="role"
+                        // onChange={handleChange}
+                        >
+                            <MenuItem value={"CUSTOMER"}>Customer</MenuItem>
+                            <MenuItem value={"RESTAURANT_OWNER"}>Restaurant Owner</MenuItem>
+                        </Field>
+                    </FormControl>
+                    <Button sx={{ mt: 2, padding: "1rem" }} fullWidth type='submit' variant='contained'>Register</Button>
+                </Form>
+            </Formik>
+            <Typography variant='body2' align='center' sx={{ mt: 3 }}>
+                If you have an account already?
+                <Button onClick={() => navigate("/account/login")}>
+                    Login
+                </Button>
+            </Typography>
+        </div >
+    )
+}
+
+export default RegisterForm
