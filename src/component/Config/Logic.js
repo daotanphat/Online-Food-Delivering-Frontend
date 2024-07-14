b339492bebab0a7f528b651a4cb982eee45bd4db
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-14 22:33:01 +0700

    feat: add function to check item is in user's favorites ot not

---
+++ src/component/Config/Logic.js
@@ -0,0 +1,8 @@
+export const isPresentInfavorites = (favorites, restaurant) => {
+    for (let item of favorites) {
+        if (item.id === restaurant.id) {
+            return true;
+        }
+    }
+    return false;
+}
