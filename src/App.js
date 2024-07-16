1c0716e0953e05bef1dd42f0398ea42f051d4c06
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-16 23:07:31 +0700

    refactor: fetch user data in App.js & add BrowseRouter

--- src/App.js
+++ src/App.js
@@ -3,26 +3,30 @@ import { Navbar } from './component/Navbar/Navbar';
 import { ThemeProvider } from '@emotion/react';
 import { darkTheme } from './theme/DarkTheme';
 import { CssBaseline } from '@mui/material';
-import Home from './component/Home/Home';
-import RestaurantDetails from './component/Restaurant/RestaurantDetails';
-import Footer from './component/Footer/Footer';
-import Cart from './component/Cart/Cart';
-import Profile from './component/Profile/Profile';
 import CustomerRouter from './Routers/CustomerRouter';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
+import { getUser } from './component/State/Authentication/Action';
+import { getCartByUserId } from './component/State/Cart/Actions';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
+  const dispatch = useDispatch()
+  const jwt = localStorage.getItem("jwt")
+  const authJwt = useSelector((state) => state.auth.jwt)
+
+  useEffect(() => {
+    dispatch(getUser(authJwt || jwt))
+    // dispatch(getCartByUserId(jwt))
+  }, [])
   return (
     <div className="App">
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        {/* <Navbar /> */}
-        {/* <Home /> */}
-        {/* <RestaurantDetails /> */}
-        {/* <Cart /> */}
-        {/* <Profile />
-        <Footer /> */}
-        <CustomerRouter />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <CustomerRouter />
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
