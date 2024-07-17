83d0f74b7babba724a9bb197aed24718fda7442a
Dao Tan Phat <daotanphat2143@gmail.com>
2024-07-17 23:32:00 +0700

    refactor: handle display user & cart of user

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,18 +1,39 @@
 import { Avatar, Badge, IconButton } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import { pink } from '@mui/material/colors';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import "./Navbar.css";
 import PersonIcon from '@mui/icons-material/Person';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { getUser, logOut } from '../State/Authentication/Action';
+import { getCartByUserId } from '../State/Cart/Actions';
 
 export const Navbar = () => {
-    const navigate = useNavigate();
+    const user = useSelector((state) => state.auth.user)
+    const cart = useSelector((state) => state.cart.cart)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleAvatarClick = () => {
+        if (user.role === "CUSTOMER") {
+            navigate("/my-profile")
+        } else {
+            navigate("/admin/restaurant")
+        }
+    }
+
+    const handleLogout = () => {
+        dispatch(logOut())
+        navigate("/")
+    }
+
     return (
         <div className='px-5 sticky top-0 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between'>
             <div className='lg:mr-10 cursor-pointer items-center space-x-4'>
-                <li className='logo font-semibold text-gray-300 text-2xl'>
+                <li onClick={() => navigate("/")} className='logo font-semibold text-gray-300 text-2xl'>
                     Phat Food
                 </li>
 
@@ -24,15 +45,20 @@ export const Navbar = () => {
                     </IconButton>
                 </div>
                 <div>
-                    {false ?
-                        <Avatar sx={{ bgcolor: "white", color: pink.A400 }}>P</Avatar>
+                    {user ?
+                        <Avatar onClick={handleAvatarClick} sx={{ bgcolor: "white", color: pink.A400 }}>{user?.fullName[0].toUpperCase()}</Avatar>
                         : <IconButton onClick={() => navigate("/account/login")}> <PersonIcon /> </IconButton>}
                 </div>
                 <div>
-                    <IconButton>
-                        <Badge color="secondary" badgeContent={3}>
+                    <IconButton onClick={() => navigate("/cart")}>
+                        <Badge color="secondary" badgeContent={cart?.cartItems.length}>
                             <ShoppingCartIcon sx={{ fontSize: "1.5rem" }} />
                         </Badge>
+                    </IconButton>
+                </div>
+                <div>
+                    <IconButton onClick={handleLogout}>
+                        {user ? <LogoutIcon sx={{ fontSize: "1.5rem" }} /> : <></>}
                     </IconButton>
                 </div>
             </div>
